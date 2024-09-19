50fd4cacf25f407a49e14e021e0cb459179007b8
Anif <anif@testpress.in>
2024-09-19 16:08:00 +0530

    Completed database setup: added models, migrations, and activated app

--- polls/views.py
+++ polls/views.py
@@ -2,3 +2,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def details(request,question_id):
+    return HttpResponse("you're looking at question %s."%question_id)
+
+def results(request,question_id):
+    response="you're looking at the result of question %s"
+    return HttpResponse(response%question_id)
+
+def vote(request,question_id):
+    return HttpResponse("you're voting on question %s."%question_id)
+
